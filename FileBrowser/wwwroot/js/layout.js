70d2bcae7ed84d1c3e86ca4b2c79db6e3812a14d
fysh711426 <fysh711426@hotmail.com>
2022-06-09 23:28:33 +0800

    clean code

---
+++ FileBrowser/wwwroot/js/layout.js
@@ -0,0 +1,29 @@
+﻿(function () {
+    var isOpen = sessionStorage.getItem('isOpen');
+    isOpen = isOpen !== null ? isOpen === "true" : true;
+
+    var btnBoard = document.getElementsByClassName('control')[0];
+    if (isOpen) 
+        btnBoard.style.display = 'block';
+
+    document.body.onclick = function () {
+        if (!isOpen) {
+            btnBoard.style.display = 'block';
+            isOpen = true;
+        }
+        else {
+            btnBoard.style.display = 'none';
+            isOpen = false;
+        }
+        sessionStorage.setItem('isOpen', isOpen);
+    };
+})();
+
+function onBack(e) {
+    history.back();
+    e.stopPropagation();
+}
+
+function onLink(e) {
+    e.stopPropagation();
+}
