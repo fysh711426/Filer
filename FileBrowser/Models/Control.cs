16303eb78e374ff759d8a0a64cb08932287a361e
fysh711426 <fysh711426@hotmail.com>
2022-06-11 14:21:35 +0800

    update _ControlPartial

---
+++ FileBrowser/Models/Control.cs
@@ -0,0 +1,11 @@
+﻿namespace FileBrowser.Models
+{
+    public class Control
+    {
+        public bool ShowHome { get; set; }
+        public Control(bool showHome = true)
+        {
+            ShowHome = showHome;
+        }
+    }
+}
