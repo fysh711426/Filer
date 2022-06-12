99b3dfb419ce8bc7005c50af399b45a26f46cb71
fysh711426 <fysh711426@hotmail.com>
2022-06-12 23:16:12 +0800

    add alert notice

--- FileBrowser/wwwroot/js/setting.js
+++ FileBrowser/wwwroot/js/setting.js
@@ -1,8 +1,45 @@
-﻿function onCheck(element) {
+﻿function initCircles(element, val) {
+    var circles = element.querySelectorAll('.theme-cicle');
+    for (var i = 0; i < circles.length; i++) {
+        var checked = circles[i].className.indexOf('checked') > -1;
+        if (circles[i].getAttribute('data-val') === val) {
+            if (!checked)
+                circles[i].className = circles[i].className + ' checked';
+        }
+        else {
+            if (checked)
+                circles[i].className = circles[i].className.replace(' checked', '');
+        }
+    }
+    var hidden = element.querySelector('input');
+    hidden.value = val;
+}
+
+function onTheme(element) {
+    var isCheck = element.className.indexOf('checked') > -1;
+    var val = element.getAttribute('data-val');
+    if (!isCheck) {
+        initCircles(element.parentNode, val);
+        storage.setTheme(val);
+        showSuccess();
+        console.log(storage.getAll());
+    }
+}
+
+function onTextTheme(element) {
+    var isCheck = element.className.indexOf('checked') > -1;
+    var val = element.getAttribute('data-val');
+    if (!isCheck) {
+        initCircles(element.parentNode, val);
+        storage.setTextTheme(val);
+        showSuccess();
+        console.log(storage.getAll());
+    }
+}
+
+function initCheck(element, isCheck) {
     var checkbox = element.querySelector('input');
     var mark = element.querySelector('.checkbox-mark');
-    var isCheck = mark.className.indexOf('checked') > -1;
-    isCheck = !isCheck;
     if (isCheck) {
         checkbox.setAttribute('checked', '');
         mark.className = mark.className + ' checked';
@@ -14,3 +51,71 @@
         mark.innerHTML = '<svg name="Checkmark" width="18" height="18" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><polyline stroke="transparent" stroke-width="2" points="3.5 9.5 7 13 15 5"></polyline></g></svg>';
     }
 }
+
+function onIsUseDeepLink(element) {
+    var mark = element.querySelector('.checkbox-mark');
+    var isCheck = mark.className.indexOf('checked') > -1;
+    isCheck = !isCheck;
+    initCheck(element, isCheck);
+    storage.setIsUseDeepLink(isCheck);
+    showSuccess();
+    console.log(storage.getAll());
+}
+
+function onDeepLinkPackage(element) {
+    var val = element.value;
+    var prev = element.getAttribute('data-prev');
+    if (prev !== val) {
+        storage.setDeepLinkPackage(val);
+        element.setAttribute('data-prev', val);
+        showSuccess();
+        console.log(storage.getAll());
+    }
+}
+
+function showSuccess() {
+    var notice = document.querySelector('.notice-warp');
+    var isShow = notice.getAttribute('data-show')
+    if (isShow === 'false') {
+        notice.innerHTML = '<div class="notice success">已儲存變更。</div>';
+        notice.className = notice.className + ' show';
+        notice.setAttribute('data-show', 'true');
+        return;
+    }
+    notice.innerHTML = '';
+    notice.className = notice.className.replace(' show', '');
+    notice.setAttribute('data-show', 'false');
+    setTimeout(function () {
+        notice.innerHTML = '<div class="notice success">已儲存變更。</div>';
+        notice.className = notice.className + ' show';
+        notice.setAttribute('data-show', 'true');
+    }, 1);
+}
+
+(function init() {
+    //----- theme -----
+    (function () {
+        var theme = storage.theme();
+        var cicles = document.querySelector('#theme');
+        initCircles(cicles, theme);
+    })();
+    //----- textTheme -----
+    (function () {
+        var textTheme = storage.textTheme();
+        var cicles = document.querySelector('#textTheme');
+        initCircles(cicles, textTheme);
+    })();
+    //----- isUseDeepLink -----
+    (function () {
+        var isUseDeepLink = storage.isUseDeepLink();
+        var checkbox = document.querySelector('#isUseDeepLink');
+        initCheck(checkbox, isUseDeepLink);
+    })();
+    //----- deepLinkPackage -----
+    (function () {
+        var deepLinkPackage = storage.deepLinkPackage();
+        var input = document.querySelector('#deepLinkPackage');
+        input.value = deepLinkPackage;
+        input.setAttribute('data-prev', deepLinkPackage);
+    })();
+})();
---
+++ FileBrowser/wwwroot/js/storage.js
@@ -0,0 +1,65 @@
+﻿var storage = null;
+(function () {
+    var IS_OPEN = 'IS_OPEN';
+    var VOLUMN = 'VOLUMN';
+    var THEME = 'THEME';
+    var TEXT_THEME = 'TEXT_THEME';
+    var IS_USE_DEEP_LINK = 'IS_USE_DEEP_LINK';
+    var DEEP_LINK_PACKAGE = 'DEEP_LINK_PACKAGE';
+
+    var _storage = {
+        getAll: function () {
+            return {
+                isOpen: sessionStorage.getItem(IS_OPEN),
+                volume: localStorage.getItem(VOLUMN),
+                theme: localStorage.getItem(THEME),
+                textTheme: localStorage.getItem(TEXT_THEME),
+                isUseDeepLink: localStorage.getItem(IS_USE_DEEP_LINK),
+                deepLinkPackage: localStorage.getItem(DEEP_LINK_PACKAGE)
+            }
+        },
+        isOpen: function () {
+            var isOpen = sessionStorage.getItem(IS_OPEN);
+            return isOpen !== null ? isOpen === "true" : true;
+        },
+        setIsOpen: function (val) {
+            sessionStorage.setItem(IS_OPEN, val);
+        },
+        volume: function () {
+            var volume = localStorage.getItem(VOLUMN);
+            return volume !== null ? parseFloat(volume) : 0.0;
+        },
+        setVolume: function (val) {
+            sessionStorage.setItem(VOLUMN, val);
+        },
+        theme: function () {
+            var theme = localStorage.getItem(THEME);
+            return theme !== null ? theme : 'dark';
+        },
+        setTheme: function (val) {
+            sessionStorage.setItem(THEME, val);
+        },
+        textTheme: function () {
+            var textTheme = localStorage.getItem(TEXT_THEME);
+            return textTheme !== null ? textTheme : 'dark';
+        },
+        setTextTheme: function (val) {
+            sessionStorage.setItem(TEXT_THEME, val);
+        },
+        isUseDeepLink: function () {
+            var isUseDeepLink = localStorage.getItem(IS_USE_DEEP_LINK);
+            return isUseDeepLink !== null ? isUseDeepLink === "true" : true;
+        },
+        setIsUseDeepLink: function (val) {
+            sessionStorage.setItem(IS_USE_DEEP_LINK, val);
+        },
+        deepLinkPackage: function () {
+            var deepLinkPackage = localStorage.getItem(DEEP_LINK_PACKAGE);
+            return deepLinkPackage !== null ? deepLinkPackage : '';
+        },
+        setDeepLinkPackage: function (val) {
+            sessionStorage.setItem(DEEP_LINK_PACKAGE, val);
+        }
+    }
+    storage = _storage;
+})();
