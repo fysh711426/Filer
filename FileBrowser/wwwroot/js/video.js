2c688749370811efc1666f70aaa88300152e2e04
fysh711426 <fysh711426@hotmail.com>
2022-06-15 22:20:27 +0800

    fix text page title

---
+++ FileBrowser/wwwroot/js/video.js
@@ -0,0 +1,26 @@
+﻿(function () {
+    var main = document.querySelector('.video-warp');
+    if (main) {
+        main.style.opacity = 1;
+    }
+
+    // player
+    var player = videojs('player', {
+        sources: [{ src: videoUrl }],
+        controls: true,
+        autoplay: false,
+        preload: 'auto',
+        fluid: true,
+        controlBar: {
+            pictureInPictureToggle: false
+        }
+    });
+    player.on('ready', function () {
+        var volume = storage.volume();
+        player.volume(volume);
+    });
+    player.on('volumechange', function () {
+        var volume = player.volume();
+        storage.setVolume(volume);
+    });
+})();
