0bb65b4ec3a2a5bd49637fa1b224fb9c6a240c31
fysh711426 <fysh711426@hotmail.com>
2022-06-15 09:40:20 +0800

    fix file icon color

--- FileBrowser/wwwroot/js/theme.js
+++ FileBrowser/wwwroot/js/theme.js
@@ -1,21 +1,19 @@
-﻿(function () {
+﻿function checkTheme() {
     function setTheme(theme) {
         if (theme !== document.body.className) {
             document.body.className = theme;
         }
+    }
+    var onThemeChange = function (newTheme) {
+        var theme = storage.theme();
+        if (newTheme !== theme)
+            storage.setTheme(newTheme);
+        setTheme(newTheme);
     }
     var theme = storage.theme();
     var darkQuery = window.matchMedia("(prefers-color-scheme: dark)");
     if (theme === '')
         theme = darkQuery.matches ? "dark" : "light";
     setTheme(theme);
-    window.onThemeChange = function (newTheme) {
-        var theme = storage.theme();
-        if (newTheme !== theme)
-            storage.setTheme(newTheme);
-        setTheme(newTheme);
-    }
-    darkQuery.addListener(function (e) {
-        window.onThemeChange(e.matches ? "dark" : "light");
-    });
-})();
+    window.onThemeChange = onThemeChange;
+}
