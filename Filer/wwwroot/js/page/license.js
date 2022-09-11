525a18ea261dceeba02aff6120f057c9c5b5170e
fysh711426 <fysh711426@hotmail.com>
2022-09-11 21:40:19 +0800

    update license

--- Filer/wwwroot/js/page/license.js
+++ Filer/wwwroot/js/page/license.js
@@ -4,7 +4,12 @@
     data: {
     },
     mounted() {
-        document.querySelector('.layout').style.opacity = 1;
+        this.theme = document.body.getAttribute('theme');
+        onThemeButtonChange(this.theme);
+        this.isLoaded = true;
+        setTimeout(function () {
+            document.querySelector('.layout').style.opacity = 1;
+        }, 1);
     },
     methods: {
     }
